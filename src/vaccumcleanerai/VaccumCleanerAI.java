9d35115cac877f30105872387bbad6f04c62f540
HypnooS <hycarooss@gmail.com>
2016-03-26 13:24:28 -0300

    printEnvironment()

--- src/vaccumcleanerai/VaccumCleanerAI.java
+++ src/vaccumcleanerai/VaccumCleanerAI.java
@@ -5,8 +5,6 @@
  */
 package vaccumcleanerai;
 
-import java.util.Random;
-
 /**
  *
  * @author zwar
@@ -21,8 +19,21 @@ public class VaccumCleanerAI {
         Environment environment = new Environment(3, 3);     //<-- cuidado com esse metrica, ela é de 1 a n, e não de 0 a n.  
         VaccumAI vaccum = new VaccumAI(0,1,2,2,environment); //<-- Aqui já é de 0 a n.
         vaccum.activeAI();
-               
-
     }
+    public void printEnvironment(Environment environment, int line, int column){
+        System.out.println("<------------------------>");
+        for(int repeat=0; repeat != (line*column); repeat++)
+        {
+            for(int m=0; m < line; m++)
+            {
+                for(int n=0; n < column; n++)
+                {
+                    
+                }
+            }
+        }
+        
+    }
+    
         
 }
